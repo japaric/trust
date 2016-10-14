3c55c748d2dff9cb7f1a14139bce46ad63c34f93
Jorge Aparicio <japaricious@gmail.com>
2016-10-14 15:04:27 -0500

    test the new targets that came up today

--- ci/run.sh
+++ ci/run.sh
@@ -1,18 +1,37 @@
 set -ex
 
 test_mode() {
-    cargo test --target $TARGET
-    cargo test --target $TARGET --release
+    cargo build $flags
+    cargo build $flags --release
 
-    cargo run --target $TARGET
-    cargo run --target $TARGET --release
+    case $TARGET in
+        s390x-unknown-linux-gnu)
+            cargo test $flags --no-run
+            cargo test $flags --no-run --release
+            ;;
+        *)
+            cargo test $flags
+            cargo test $flags --release
+            cargo run $flags
+            cargo run $flags --release
+            ;;
+    esac
 }
 
 deploy_mode() {
-    cargo rustc --target $TARGET --release --bin hello -- -C lto
+    cargo rustc $flags --release --bin hello -- -C lto
 }
 
 run() {
+    case $TARGET in
+        arm*-unknown-linux-musleabi*)
+            flags="--no-default-features --target $TARGET"
+            ;;
+        *)
+            flags="--target $TARGET"
+            ;;
+    esac
+
     if [ -z $TRAVIS_TAG ]; then
         test_mode
     else
