d0c1dda3dab942e314cdae547546c410813a66c1
Jorge Aparicio <japaricious@gmail.com>
2016-10-14 16:25:56 -0500

    appveyor: compile natively

--- ci/install.ps1
+++ ci/install.ps1
@@ -1,25 +1,17 @@
 if ($Env:TARGET -Match 'gnu') {
-    $Env:HOST = 'x86_64-pc-windows-gnu'
-
     if ($Env:TARGET -Match 'x86_64') {
         $Env:PATH += ';C:\msys64\mingw64\bin'
     } else {
         $Env:PATH += ';C:\msys64\mingw32\bin'
     }
-} else {
-    $Env:HOST = 'x86_64-pc-windows-msvc'
 }
 
 [Net.ServicePointManager]::SecurityProtocol = 'Ssl3, Tls, Tls12'
 Start-FileDownload 'https://win.rustup.rs' 'rustup-init.exe'
 
-.\rustup-init --default-host $Env:HOST --default-toolchain $Env:RUST_VERSION -y
+.\rustup-init --default-host $Env:TARGET --default-toolchain $Env:RUST_VERSION -y
 
 $Env:PATH = 'C:\Users\appveyor\.cargo\bin;' + $Env:PATH
-
-if ($Env:TARGET -ne $Env:HOST) {
-    rustup target add $Env:TARGET
-}
 
 rustc -Vv
 
