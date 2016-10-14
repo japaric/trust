d910340d43a9090396e384817e78960abcdcc432
Jorge Aparicio <japaricious@gmail.com>
2016-10-13 23:50:43 -0500

    enable the powerpc targets

--- ci/run.sh
+++ ci/run.sh
@@ -3,17 +3,10 @@ set -ex
 run() {
     local target=$1
 
-    case $target in
-        powerpc64le-unknown-linux-gnu)
-            cargo test --target $target --no-run
-            ;;
-        *)
-            cargo test --target $target
-            cargo test --target $target --release
-            cargo run --target $target
-            cargo run --target $target --release
-            ;;
-    esac
+    cargo test --target $target
+    cargo test --target $target --release
+    cargo run --target $target
+    cargo run --target $target --release
 }
 
 run $1
