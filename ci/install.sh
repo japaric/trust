98677e232cc587d903a417a787f4f8ca76836ef8
Jorge Aparicio <japaricious@gmail.com>
2016-12-31 21:19:59 -0500

    try to extend PATH using powershell

--- ci/install.sh
+++ ci/install.sh
@@ -6,17 +6,19 @@ main() {
     curl https://sh.rustup.rs -sSf | \
         sh -s -- -y --default-toolchain $TRAVIS_RUST_VERSION
 
-    if [ $TRAVIS_OS_NAME = linux ]; then
-        curl -LSfs http://japaric.github.io/trust/install.sh | \
-            sh -s -- \
-               --force \
-               --git japaric/cross \
-               --tag v$version \
-               --target x86_64-unknown-linux-musl \
-               --to ~/.cargo/bin
-    else
-        cargo install cross -f --vers $version
-    fi
+    cargo install -f --git https://github.com/japaric/cross
+
+    # if [ $TRAVIS_OS_NAME = linux ]; then
+    #     curl -LSfs http://japaric.github.io/trust/install.sh | \
+    #         sh -s -- \
+    #            --force \
+    #            --git japaric/cross \
+    #            --tag v$version \
+    #            --target x86_64-unknown-linux-musl \
+    #            --to ~/.cargo/bin
+    # else
+    #     cargo install cross -f --vers $version
+    # fi
 }
 
 main
