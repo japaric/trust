dcfad00ecdfbee35e760a682b5d8f06602b192d1
Jorge Aparicio <japaricious@gmail.com>
2017-01-08 13:20:58 -0500

    ci/script.sh: reduce rightward drift

--- ci/script.sh
+++ ci/script.sh
@@ -7,13 +7,15 @@ main() {
     cross build --target $TARGET
     cross build --target $TARGET --release
 
-    if [ -z $DISABLE_TESTS ]; then
-        cross test --target $TARGET
-        cross test --target $TARGET --release
-
-        cross run --target $TARGET
-        cross run --target $TARGET --release
+    if [ -n $DISABLE_TESTS ]; then
+        return
     fi
+
+    cross test --target $TARGET
+    cross test --target $TARGET --release
+
+    cross run --target $TARGET
+    cross run --target $TARGET --release
 }
 
 # we don't run the "test phase" when doing deploys
