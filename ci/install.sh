95ab97ec622b31795db1fc95cb146812210907d5
Benjamin Hoffmeyer <hoffmeyer25@gmail.com>
2017-04-09 15:28:31 -0400

    Stop installing rust with rustup.rs twice
    Since https://github.com/travis-ci/travis-ci/issues/7510 Travis now uses rustup.rs to install rust. This change will stop trust from installing rust a second time the exact same way that Travis does.

--- ci/install.sh
+++ ci/install.sh
@@ -1,9 +1,6 @@
 set -ex
 
 main() {
-    curl https://sh.rustup.rs -sSf | \
-        sh -s -- -y --default-toolchain $TRAVIS_RUST_VERSION
-
     local target=
     if [ $TRAVIS_OS_NAME = linux ]; then
         target=x86_64-unknown-linux-gnu
