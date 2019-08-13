5f4c85cc3ac15811fba8452d098a9e8281b67039
Oleksii Filonenko <brightone@protonmail.com>
2019-08-13 22:13:34 +0300

    Shellcheck ci/before_deploy.sh

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -3,10 +3,9 @@
 set -ex
 
 main() {
-    local src=$(pwd) \
-          stage=
+    src=$(pwd)
 
-    case $TRAVIS_OS_NAME in
+    case "$TRAVIS_OS_NAME" in
         linux)
             stage=$(mktemp -d)
             ;;
@@ -18,16 +17,16 @@ main() {
     test -f Cargo.lock || cargo generate-lockfile
 
     # TODO Update this to build the artifacts that matter to you
-    cross rustc --bin hello --target $TARGET --release -- -C lto
+    cross rustc --bin hello --target "$TARGET" --release -- -C lto
 
     # TODO Update this to package the right artifacts
-    cp target/$TARGET/release/hello $stage/
+    cp "target/$TARGET/release/hello" "$stage/"
 
-    cd $stage
-    tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
-    cd $src
+    cd "$stage"
+    tar czf "$src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz" ./*
+    cd "$src"
 
-    rm -rf $stage
+    rm -rf "$stage"
 }
 
 main
