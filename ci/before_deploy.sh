fcb87472686481382b915c8de653f7f77d69b49e
Praveen Perera <praveenperera@users.noreply.github.com>
2020-09-05 21:39:28 -0400

    Fix for breaking builds on > rust 1.45
    ref: https://github.com/rust-lang/rust/issues/71848

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -18,7 +18,7 @@ main() {
     test -f Cargo.lock || cargo generate-lockfile
 
     # TODO Update this to build the artifacts that matter to you
-    cross rustc --bin hello --target $TARGET --release -- -C lto
+    cross rustc --bin hello --target $TARGET --release
 
     # TODO Update this to package the right artifacts
     cp target/$TARGET/release/hello $stage/
